7b230713b21588e8627b899ebe7a153592b7ad68
dani <daniel.valdecantos@gmail.com>
2023-05-11 09:43:42 -0700

    fix: association in spot image model

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -3,7 +3,7 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class SpotImage extends Model {
     static associate(models) {
-      SpotImage.models(models.Spot, { foreignKey: "spotId" });
+      SpotImage.belongsTo(models.Spot, { foreignKey: "spotId" });
     }
   }
   SpotImage.init(
