4a53ba688123af78dc9d7062e0a9b1a47f66d074
dani <daniel.valdecantos@gmail.com>
2023-05-11 10:04:05 -0700

    feat: add booking associations

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -2,7 +2,10 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Booking extends Model {
-    static associate(models) {}
+    static associate(models) {
+      Booking.belongsTo(models.User);
+      Booking.belongsTo(models.Spot);
+    }
   }
   Booking.init(
     {
