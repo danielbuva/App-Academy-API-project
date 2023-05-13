a93075753390f00647a5b8c5b444ed750ddce441
dani <daniel.valdecantos@gmail.com>
2023-05-13 12:38:09 -0700

    feat: add seed data for spots (✿◡‿◡)

---
+++ backend/db/seeders/20230513184502-spots-data.js
@@ -0,0 +1,66 @@
+"use strict";
+const spots = [
+  {
+    ownerId: 1,
+    address: "101 Snake Eyes Dr.",
+    city: "Denver",
+    state: "Colorado",
+    country: "US",
+    lat: 39.7392,
+    lng: 104.9903,
+    name: "Pair of Dice",
+    description:
+      "Welcome to Pair of Dice. You will enjoy your stay! （￣︶￣）↗",
+    price: 1000000,
+  },
+  {
+    ownerId: 2,
+    address: "420 W Riverside Ave #113",
+    city: "Seattle",
+    state: "Washington",
+    country: "US",
+    lat: 47.6062,
+    lng: 122.3321,
+    name: "Home Away From Home",
+    description:
+      "Welcome to your Home Away From Home. You will enjoy your stay! (✿◡‿◡)",
+    price: 690000,
+  },
+  {
+    ownerId: 3,
+    address: "1969 Isabel St",
+    city: "Los Angeles",
+    state: "California",
+    country: "US",
+    lat: 34.0522,
+    lng: 118.2437,
+    name: "cool house",
+    description:
+      "Welcome to cool house. You will enjoy your stay! (*^_^*)",
+    price: 10,
+  },
+];
+
+let options = { tableName: "Spots" };
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(options, spots);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete(options, {
+      name: {
+        [Sequelize.Op.in]: [
+          "Pair of Dice",
+          "Home Away From Home",
+          "cool house",
+        ],
+      },
+    });
+  },
+};
