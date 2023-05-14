0ecc893238d6f4160e8a05ee8d3391fcfd347356
dani <daniel.valdecantos@gmail.com>
2023-05-14 12:39:09 -0700

    fix: include first/last name to match specs

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -14,6 +14,8 @@ router.get("/", (req, res) => {
   if (user) {
     const safeUser = {
       id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
       username: user.username,
     };
