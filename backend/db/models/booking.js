a52408066de4180018d3cc15943d49924c7acd31
dani <daniel.valdecantos@gmail.com>
2023-05-15 15:18:38 -0700

    fix: add foreign keys to bookings model

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -3,8 +3,14 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Booking extends Model {
     static associate(models) {
-      Booking.belongsTo(models.User);
-      Booking.belongsTo(models.Spot);
+      Booking.belongsTo(models.User, {
+        foreignKey: "userId",
+        onDelete: "CASCADE",
+      });
+      Booking.belongsTo(models.Spot, {
+        foreignKey: "userId",
+        onDelete: "CASCADE",
+      });
     }
   }
   Booking.init(
