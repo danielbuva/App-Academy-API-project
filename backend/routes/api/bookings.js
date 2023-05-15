d770df109572b2497bbfca9662f86bce2a30abb6
dani <daniel.valdecantos@gmail.com>
2023-05-14 23:49:15 -0700

    feat: update put booking endpoint to match specs

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,7 +1,7 @@
 const { verifyAuth } = require("../../services/auth.server");
 const { invariant } = require("../../services/error.server");
 const { Booking, Spot } = require("../../db/models");
-const { literal } = require("sequelize");
+const { literal, Op } = require("sequelize");
 const router = require("express").Router();
 
 router.get("/current", verifyAuth, async (req, res) => {
@@ -33,20 +33,67 @@ router.get("/current", verifyAuth, async (req, res) => {
 router.put("/:bookingId", verifyAuth, async (req, res) => {
   const { startDate, endDate } = req.body;
   const userId = req.user.id;
+  const date = new Date();
+  const today = `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()}`;
 
-  //@TODO: add date validation helper function from review router
-  //@TODO: add date conflicts validation helper function
-  //@TODO: add date past validation helper function
-
-  const booking = await Booking.findByPk(req.params.bookingId, {
-    where: { userId },
+  const booking = await Booking.findOne({
+    attributes: { include: ["id"] },
+    where: { userId, id: req.params.bookingId },
+  });
+  invariant(booking, "Booking couldn't be found");
+  if (booking.endDate >= today) {
+    return res
+      .status(403)
+      .json({ message: "Past bookings can't be modified" });
+  }
+  const bookingConflicts = await Booking.findOne({
+    where: {
+      [Op.or]: [
+        {
+          startDate: {
+            [Op.between]: [startDate, endDate],
+          },
+        },
+        {
+          endDate: {
+            [Op.between]: [startDate, endDate],
+          },
+        },
+        {
+          [Op.and]: [
+            {
+              startDate: {
+                [Op.lte]: startDate,
+              },
+            },
+            {
+              endDate: {
+                [Op.gte]: endDate,
+              },
+            },
+          ],
+        },
+      ],
+      userId: { [Op.not]: userId },
+    },
   });
 
-  invariant(booking, "Booking couldn't be found");
+  console.log({ bookingConflicts });
 
-  booking.startDate = startDate;
-  booking.endDate = endDate;
-  await booking.save();
+  if (bookingConflicts) {
+    return res.status(403).json({
+      message:
+        "Sorry, this spot is already booked for the specified dates",
+      errors: {
+        startDate: "Start date conflicts with an existing booking",
+        endDate: "End date conflicts with an existing booking",
+      },
+    });
+  }
+
+  await booking.update({ startDate, endDate });
 
   res.json(booking);
 });
