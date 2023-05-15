b2db875c25339528f259334d341c8e5892c8b46e
dani <daniel.valdecantos@gmail.com>
2023-05-15 10:42:42 -0700

    fix: delete spot image

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -8,45 +8,41 @@ const {
 const { verifyAuth } = require("../../services/auth.server");
 const { invariant } = require("../../services/error.server");
 const router = require("express").Router();
+const { Op } = require("sequelize");
 
-router.delete(
-  "/spot-images/:imageId",
-  verifyAuth,
-  async (req, res, next) => {
-    const userId = req.user.id;
-    const spotImage = await SpotImage.findByPk(req.params.imageId, {
-      include: {
-        model: Spot,
-        required: true,
-        include: { model: User, required: true, where: { id: userId } },
-      },
-    });
-    invariant(spotImage, "Spot Image couldn't be found", next);
+router.delete("/spot-images/:imageId", verifyAuth, async (req, res) => {
+  const userId = req.user.id;
+  const spotImage = await SpotImage.findOne({
+    attributes: ["id"],
+    where: {
+      [Op.and]: [{ id: req.params.imageId }, { "$Spot.ownerId$": userId }],
+    },
+    include: {
+      model: Spot,
+      required: true,
+      attributes: [],
+    },
+  });
+  invariant(spotImage, "Spot Image couldn't be found");
 
-    await spotImage.destroy();
+  await spotImage.destroy();
+  res.json({ message: "Successfully deleted" });
+});
 
-    res.json({ message: "Successfully deleted" });
-  }
-);
+router.delete("review-images/:imageId", verifyAuth, async (req, res) => {
+  const userId = req.user.id;
+  const reviewImage = await ReviewImage.findByPk(req.params.imageId, {
+    include: {
+      model: Review,
+      required: true,
+      include: { model: User, required: true, where: { id: userId } },
+    },
+  });
+  invariant(reviewImage, "Review Image couldn't be found");
 
-router.delete(
-  "review-images/:imageId",
-  verifyAuth,
-  async (req, res, next) => {
-    const userId = req.user.id;
-    const reviewImage = await ReviewImage.findByPk(req.params.imageId, {
-      include: {
-        model: Review,
-        required: true,
-        include: { model: User, required: true, where: { id: userId } },
-      },
-    });
-    invariant(reviewImage, "Review Image couldn't be found", next);
+  await reviewImage.destroy();
 
-    await reviewImage.destroy();
-
-    res.json({ message: "Successfully deleted" });
-  }
-);
+  res.json({ message: "Successfully deleted" });
+});
 
 module.exports = router;
