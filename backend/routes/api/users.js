8440e075c06d600de7d6e8dfd000f4579c86534a
dani <daniel.valdecantos@gmail.com>
2023-05-16 09:27:16 -0700

    feat: add get all users route to test prod

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,12 @@
 const { signup, validateSignup } = require("../../services/auth.server");
 const router = require("express").Router();
+const { User } = require("../../db/models");
 
 router.post("/", validateSignup, signup);
+
+router.get("/", async (_, res) => {
+  const users = await User.findAll();
+  res.json(users);
+});
 
 module.exports = router;
