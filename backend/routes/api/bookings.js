6679697faca3c7a5b2b55a510218e72923f91a0e
dani <daniel.valdecantos@gmail.com>
2023-05-17 10:03:59 -0700

    refactor: edit booking endpoint

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,11 +1,13 @@
 const {
   invariant,
   checkAuthorization,
+  throwError,
 } = require("../../services/error.server");
+const { validateBooking } = require("../../services/validation.server");
 const { verifyAuth } = require("../../services/auth.server");
 const { Booking, Spot } = require("../../db/models");
-const { literal, Op } = require("sequelize");
 const router = require("express").Router();
+const { literal } = require("sequelize");
 const { today } = require("../../utils");
 
 router.get("/current", verifyAuth, async (req, res) => {
@@ -34,65 +36,24 @@ router.get("/current", verifyAuth, async (req, res) => {
 
 router.put("/:bookingId", verifyAuth, async (req, res) => {
   const { startDate, endDate } = req.body;
-  const userId = req.user.id;
 
   const booking = await Booking.findOne({
     attributes: { include: ["id"] },
-    where: { userId, id: req.params.bookingId },
+    where: { id: req.params.bookingId },
   });
   invariant(booking, "Booking couldn't be found");
-  if (booking.endDate >= today()) {
-    throwError(403, "Past bookings can't be modified");
-  } 
-  const bookingConflicts = await Booking.findOne({
-    where: {
-      [Op.or]: [
-        {
-          startDate: {
-            [Op.between]: [startDate, endDate],
-          },
-        },
-        {
-          endDate: {
-            [Op.between]: [startDate, endDate],
-          },
-        },
-        {
-          [Op.and]: [
-            {
-              startDate: {
-                [Op.lte]: startDate,
-              },
-            },
-            {
-              endDate: {
-                [Op.gte]: endDate,
-              },
-            },
-          ],
-        },
-      ],
-      userId: { [Op.not]: userId },
-    },
-  });
+  checkAuthorization(booking.userId === req.userId);
 
-  if (bookingConflicts) {
-    return res.status(403).json({
-      message:
-        "Sorry, this spot is already booked for the specified dates",
-      errors: {
-        startDate: "Start date conflicts with an existing booking",
-        endDate: "End date conflicts with an existing booking",
-      },
-    });
-  }
+  await Promise.all([
+    validateBooking(startDate, endDate, booking.spotId, booking.endDate),
+    booking.update({ startDate, endDate }),
+  ]);
 
-  await booking.update({ startDate, endDate });
   res.json(booking);
 });
 
 router.delete("/:bookingId", verifyAuth, async (req, res, next) => {
-  const booking = await Booking.findByPl(req.params.bookingId);
+  const booking = await Booking.findByPk(req.params.bookingId);
   invariant(booking, "Booking coudn't be found");
   checkAuthorization(booking.userId === req.user.id);
 
@@ -102,7 +63,7 @@ router.delete("/:bookingId", verifyAuth, async (req, res, next) => {
   if (bookingConflicts) {
     throwError(402, "Bookings that have been started can't be deleted");
   }
-    
+
   await booking.destroy();
   res.json({ message: "Successfully deleted" });
 });
