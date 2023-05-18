3742316bbae76dd94c87464cd52c5a2b0025b594
dani <daniel.valdecantos@gmail.com>
2023-05-18 10:43:23 -0700

    fix: add merge params true

--- backend/routes/api/bookings/id.js
+++ backend/routes/api/bookings/id.js
@@ -1,6 +1,6 @@
 const { bookings } = require("./lib");
 
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
 
 router.put("/", bookings.edit);
 router.delete("/", bookings.delete);
