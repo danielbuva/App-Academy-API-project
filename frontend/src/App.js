411589c4b33068348153f5bdd82047129a701483
danielbuva <daniel.valdecantos@gmail.com>
2023-06-01 21:45:39 -0500

    feat: initialize frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>olá from app</h1>;
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./index.css";
+
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+import configureStore from "./store";
+import { csrfFetch, restoreCSRF } from "./store/csrf";
+import * as sessionActions from "./store/session";
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
