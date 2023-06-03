f4e89705e8cae7bab34b1cb500093511609d02c1
danielbuva <daniel.valdecantos@gmail.com>
2023-06-03 01:18:02 -0700

    feat: add menu css

--- frontend/src/components/Nav/UserMenu/Menu/MenuButton/index.jsx
+++ frontend/src/components/Nav/UserMenu/Menu/MenuButton/index.jsx
@@ -5,14 +5,13 @@ function MeniButton({ ref, setShow, show }) {
   return (
     <div
       style={{
-        width: "60px",
+        width: "58px",
         padding: "5px 5px 5px 12px",
-        // height: "42px",
         cursor: "pointer",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
-        border: "solid #3A3E41",
+        border: "solid #DDDDDD 1px",
         borderRadius: "21px",
       }}
       onClick={() => setShow(!show)}
