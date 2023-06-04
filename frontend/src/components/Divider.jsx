cc136fc81d1701679a678e90e920c6d9be7bd10d
danielbuva <daniel.valdecantos@gmail.com>
2023-06-03 19:53:18 -0700

    style: change defaults of divider

--- frontend/src/components/Divider.jsx
+++ frontend/src/components/Divider.jsx
@@ -1,10 +1,10 @@
-function Divider({ margin = 7 }) {
+function Divider({ margin = 0, color = "#ebebeb" }) {
   return (
     <div
       id="divider"
       style={{
         margin: `${margin}px 0px ${margin}px 0px`,
-        borderTop: "solid 1px #ebebeb ",
+        borderTop: `solid 1px color`,
       }}
     />
   );
