7711e1f22236e054756e3aabc07b7693d4c8b45a
dani <daniel.valdecantos@gmail.com>
2023-06-04 23:44:58 -0700

    fix: import express

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const { isProduction } = require("../config");
 const {
   // serveHtml,
