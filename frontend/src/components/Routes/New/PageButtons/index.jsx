8c371f186ad443a77476af125b50370ee9464856
dani <daniel.valdecantos@gmail.com>
2023-06-06 16:57:07 -0700

    feat: add page buttons to create a new spot form

---
+++ frontend/src/components/Routes/New/PageButtons/index.jsx
@@ -0,0 +1,12 @@
+import "./PageButtons.css";
+
+function PageButtons() {
+  return (
+    <div id="page-buttons">
+      <button className="page-buttons back">Back</button>
+      <button className="page-buttons next">Next</button>
+    </div>
+  );
+}
+
+export default PageButtons;
