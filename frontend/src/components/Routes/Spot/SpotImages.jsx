824e0f262e6dded7bffaf5fbcde2d1e783f8b64d
dani <daniel.valdecantos@gmail.com>
2023-06-05 21:41:24 -0700

    style: add styling to spot images

--- frontend/src/components/Routes/Spot/SpotImages.jsx
+++ frontend/src/components/Routes/Spot/SpotImages.jsx
@@ -1,15 +1,51 @@
 import { useSelector } from "react-redux";
 
+import "./Spot.css";
+
 function SpotImages() {
   const spot = useSelector((state) => state.spots.spot);
 
   if (!spot || !spot.SpotImages) return null;
 
   return (
-    <div style={{ width: "100%" }}>
-      {spot.SpotImages.map((image) => (
-        <img src={image.url} alt={image.url} key={image.id} />
-      ))}
+    <div id="images">
+      {spot.SpotImages[0] && (
+        <img
+          src={spot.SpotImages[0].url}
+          alt={spot.SpotImages[0].url}
+          id="main-image"
+        />
+      )}
+      <div id="right-images">
+        {spot.SpotImages[1] && (
+          <img
+            src={spot.SpotImages[1].url}
+            alt={spot.SpotImages[1].url}
+            className="image-section"
+          />
+        )}
+        {spot.SpotImages[2] && (
+          <img
+            src={spot.SpotImages[2].url}
+            alt={spot.SpotImages[2].url}
+            className="image-section"
+          />
+        )}
+        {spot.SpotImages[3] && (
+          <img
+            src={spot.SpotImages[3].url}
+            alt={spot.SpotImages[3].url}
+            className="image-section"
+          />
+        )}
+        {spot.SpotImages[4] && (
+          <img
+            src={spot.SpotImages[4].url}
+            alt={spot.SpotImages[4].url}
+            className="image-section"
+          />
+        )}
+      </div>
     </div>
   );
 }
