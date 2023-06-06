7eff954f5401791d1a939d6599bc1db74f4f59e8
dani <daniel.valdecantos@gmail.com>
2023-06-06 08:32:23 -0700

    style: make chevrons smaller

--- frontend/src/components/Routes/Spots/Preview/ChevronIcons/ChevronRight.jsx
+++ frontend/src/components/Routes/Spots/Preview/ChevronIcons/ChevronRight.jsx
@@ -1,7 +1,7 @@
 import Chevron from "./ChevronRight.svg";
 
 function ChevronRight() {
-  return <img src={Chevron} alt="right" style={{ width: "12px" }} />;
+  return <img src={Chevron} alt="right" style={{ width: "10px" }} />;
 }
 
 export default ChevronRight;
