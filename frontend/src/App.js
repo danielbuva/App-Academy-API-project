edd90b98f0f0e61aa29ff273801863a8d70288ce
dani <daniel.valdecantos@gmail.com>
2023-06-06 16:54:22 -0700

    feat: add new routes for current and new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import { Route, Switch } from "react-router-dom";
 import Spots from "./components/Routes/Spots/Spots";
 import Spot from "./components/Routes/Spot";
 import Layout from "./components/Layout";
+import NewSpotForm from "./components/Routes/New/NewSpotForm";
 
 function App() {
   return (
     <Layout>
       <Switch>
         <Route exact path="/" component={Spots} />
+        <Route path="/spots/new" component={NewSpotForm} />
+        <Route path="/spots/current" component={Spot} />
         <Route path="/spots/:id" component={Spot} />
       </Switch>
     </Layout>
