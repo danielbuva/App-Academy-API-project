9632e2c9994aaa3233cbb87edbd2737b4c6b4491
dani <daniel.valdecantos@gmail.com>
2023-06-05 20:44:48 -0700

    feat: start spot id route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import Spots from "./components/Home/Spots/Spots";
+import { Route, Switch } from "react-router-dom";
+import Spots from "./components/Routes/Spots/Spots";
+import Spot from "./components/Routes/Spot";
 import Layout from "./components/Layout";
 
 function App() {
   return (
     <Layout>
-      <Spots />
+      <Switch>
+        <Route exact path="/" component={Spots} />
+        <Route path="/spots/:id" component={Spot} />
+      </Switch>
     </Layout>
   );
 }
