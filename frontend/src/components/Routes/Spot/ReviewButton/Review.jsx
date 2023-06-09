877eed26054a8a5b4b635c29c791920940abadff
dani <daniel.valdecantos@gmail.com>
2023-06-09 14:21:48 -0700

    style: delete reviews

--- frontend/src/components/Routes/Spot/ReviewButton/Review.jsx
+++ frontend/src/components/Routes/Spot/ReviewButton/Review.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import Stars from "./Stars";
@@ -10,14 +10,17 @@ import {
 } from "../../../../store/reviews";
 import { useModalContext } from "../../../../hooks/useModalContext";
 import useSessionUser from "../../../../hooks/useSessionUser";
+import Divider from "../../../Divider";
 
 function Review({ id, spotId }) {
   const reviewToEdit = useSelector((s) => s.reviews.review);
   const [review, setReview] = useState(reviewToEdit.review ?? "");
   const [stars, setStars] = useState(reviewToEdit.stars ?? 0);
+  const [showDelete, setShowDelete] = useState(false);
   const dispatch = useDispatch();
   const { closeModal } = useModalContext();
   const user = useSessionUser();
+  const contentRef = useRef(null);
 
   const isEditing = reviewToEdit?.stars;
 
@@ -29,6 +32,7 @@ function Review({ id, spotId }) {
   const handleDelete = () => {
     dispatch(deleteReview(id, spotId));
     closeModal();
+    setShowDelete(false);
   };
 
   const handleUpdate = () => {
@@ -37,6 +41,22 @@ function Review({ id, spotId }) {
     );
     closeModal();
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        contentRef.current &&
+        !contentRef.current.contains(event.target)
+      ) {
+        setShowDelete(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div id="review-content">
@@ -48,8 +68,15 @@ function Review({ id, spotId }) {
       />
 
       <Stars stars={stars} setStars={setStars} />
-      <div>
-        {isEditing && <button onClick={handleDelete}>Delete</button>}
+      <div id="review-buttons">
+        {isEditing && (
+          <button
+            id="delete-review-button"
+            onClick={() => setShowDelete(!showDelete)}
+          >
+            Delete
+          </button>
+        )}
         <button
           className="spot-button"
           onClick={isEditing ? handleUpdate : handleClick}
@@ -58,6 +85,20 @@ function Review({ id, spotId }) {
           {isEditing ? "Update" : "Submit your review"}
         </button>
       </div>
+      {showDelete && (
+        <div id="confirm-delete-review" ref={contentRef}>
+          <p id="confirm-text">Are you sure you want to delete?</p>
+          <Divider />
+          <div id="confirm-buttons">
+            <button onClick={handleDelete} className="spot-button">
+              yes (delete)
+            </button>
+            <button onClick={() => setShowDelete(false)} id="confirm-no">
+              no (go back)
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
