caa19674fa31510c889e55def0566dfa963b606b
dani <daniel.valdecantos@gmail.com>
2023-06-09 08:08:51 -0700

    feat: update spot reducer

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -2,13 +2,11 @@ import { csrfFetch } from "./csrf";
 
 const GET_ALL_SPOTS = "spot/getAllSpots";
 const GET_SPOT = "spot/getSpot";
-const NEW_SPOT = "spot/newSpot";
 const DELETE_SPOT = "spot/deleteSpot";
 const UPDATE_REVIEWS = "spot/updateReviews";
 
 const setSpots = (spots) => ({ type: GET_ALL_SPOTS, payload: spots });
 const setSpot = (spot) => ({ type: GET_SPOT, payload: spot });
-const newSpot = (spot) => ({ type: NEW_SPOT, payload: spot });
 const removeSpot = (id) => ({ type: DELETE_SPOT, payload: id });
 export const updateReviews = (stars) => ({
   type: UPDATE_REVIEWS,
@@ -90,7 +88,7 @@ export const addSpotImage = (spotData, imageData) => async (dispatch) => {
         }
       }
     }
-    dispatch(newSpot(spotData));
+    dispatch(setSpot(spotData));
     return spotData.id;
   }
 };
@@ -98,31 +96,19 @@ export const addSpotImage = (spotData, imageData) => async (dispatch) => {
 const initialState = { allSpots: [], spot: {} };
 
 const spotReducer = (state = initialState, action) => {
-  let newState;
   switch (action.type) {
     case GET_ALL_SPOTS:
-      newState = { allSpots: [], ...state.spot };
-      newState.allSpots = action.payload;
-      return newState;
+      return { allSpots: action.payload, ...state.spot };
     case GET_SPOT:
-      newState = { ...state.allSpots, spot: {} };
-      newState.spot = action.payload;
-      return newState;
-    case NEW_SPOT:
-      newState = state;
-      // newState.allSpots = [...state.allSpots, action.payload];
-      newState.spot = action.payload;
-      return newState;
+      return { ...state.allSpots, spot: action.payload };
     case DELETE_SPOT:
-      newState = state.allSpots.filter(
-        (spot) => spot.id !== action.payload
-      );
-      return { ...state, allSpots: newState };
+      return {
+        ...state,
+        allSpots: state.allSpots.filter(
+          (spot) => spot.id !== action.payload
+        ),
+      };
     case UPDATE_REVIEWS:
-      console.log(
-        `average of ${state.spot.avgStarRating} and ${action.payload}`,
-        (state.spot.avgStarRating + action.payload) / 2
-      );
       return {
         ...state.allSpots,
         spot: {
@@ -134,7 +120,6 @@ const spotReducer = (state = initialState, action) => {
               : action.payload,
         },
       };
-
     default:
       return state;
   }
