08c7b3499ee9722616e82882ca4c01fe8046793d
dani <daniel.valdecantos@gmail.com>
2023-06-10 00:19:14 -0700

    fix: make review data accept longer values
    need these fixed to work with postgresql ￣へ￣

--- backend/db/migrations/20230514185040-create-review.js
+++ backend/db/migrations/20230514185040-create-review.js
@@ -39,7 +39,7 @@ module.exports = {
           onDelete: "CASCADE",
         },
         review: {
-          type: Sequelize.STRING,
+          type: Sequelize.STRING(500),
           allowNull: false,
         },
         stars: {
