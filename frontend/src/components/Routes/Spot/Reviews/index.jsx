15f8f0e62fa944b5eba084a7959f3bc3a5e8e3d1
dani <daniel.valdecantos@gmail.com>
2023-06-09 23:37:26 -0700

    fix: sort reviews by updated at

--- frontend/src/components/Routes/Spot/Reviews/index.jsx
+++ frontend/src/components/Routes/Spot/Reviews/index.jsx
@@ -21,8 +21,11 @@ function Reviews() {
   return (
     <div id="reviews">
       {reviews
-        .slice()
-        .reverse()
+        .sort((a, b) => {
+          const dateA = new Date(a.updatedAt);
+          const dateB = new Date(b.updatedAt);
+          return dateB - dateA;
+        })
         .map((r) => (
           <Review
             key={r.id}
